b687aaa367f5afd01973c1af67cf2ab4ce665e2e
satorukomatsu <sk.comiu@gmail.com>
2025-04-04 14:14:48 +0900

    映画リストコンポーネントの追加

---
+++ src/component/molecules/MovieListContent.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import MovieImgButton from "../atoms/MovieImgButton";
+import { Box } from "@mui/material";
+import "../../css/MovieListContent.css"
+
+type Props = {
+    movieList: {
+        adult: boolean,
+        backdrop_path: string | null,
+        genre_ids: number[],
+        id: number,
+        original_language: string,
+        original_title: string,
+        overview: string,
+        popularity: number,
+        poster_path: string | null,
+        release_date: string,
+        title: string,
+        video: boolean,
+        vote_average: number,
+        vote_count: number
+    }[]
+}
+
+const MovieListContent: React.FC<Props> = ({movieList}) => {
+    const onClickMovieImg = () => {}
+    return (
+        <Box className="movieListBox">
+            {movieList.map((movie) => {
+                return (
+                    <Box className="movieContentBox" key={movie.id}>
+                        <MovieImgButton
+                            srcUrl={movie.poster_path ? movie.poster_path : undefined}
+                            alt={movie.title}
+                            onClick={onClickMovieImg}
+                        />
+                    </Box>
+                )
+            })}
+        </Box>
+    )
+}
+
+export default MovieListContent
