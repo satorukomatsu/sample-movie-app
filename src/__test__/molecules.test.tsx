9ca27578b2788c902e50e22281fe1b6890db3254
satorukomatsu <sk.comiu@gmail.com>
2025-04-05 17:08:50 +0900

    検索結果の映画リスト表示を追加

--- src/__test__/molecules.test.tsx
+++ src/__test__/molecules.test.tsx
@@ -1,12 +1,7 @@
 import { fireEvent, render, screen } from '@testing-library/react';
 import MovieSearchBox from '../component/molecules/MovieSearchBox';
 import MovieListContent from '../component/molecules/MovieListContent';
-// import DialogMovieTitle from '../component/atoms/DialogMovieTitle';
-// import MovieImgButton from '../component/atoms/MovieImgButton';
-// import DialogMovieImg from '../component/atoms/DialogMovieImg';
-// import DialogMovieDescription from '../component/atoms/DialogMovieDescription';
-// import SearchTextInput from '../component/atoms/SearchTextInput';
-// import SearchButton from '../component/atoms/SearchButton';
+import SearchResultContent from '../component/molecules/SearchResultContent';
 
 describe("molecules test", () => {
     test("render Movie Search Box", () => {
@@ -45,6 +40,33 @@ describe("molecules test", () => {
                 }
         ];
         render(<MovieListContent movieList={movieListData} />);
+        const buttons = screen.getAllByRole("button");
+        expect(buttons).toBeTruthy();
+    });
+
+    test("render Search Result Content", () => {
+        const movieListData = [
+                {
+                    "adult": false,
+                    "backdrop_path": "/uOQgMhYyu7dkXdHoRkCqZIF32M6.jpg",
+                    "genre_ids": [
+                        28,
+                        12
+                    ],
+                    "id": 1241320,
+                    "original_language": "ja",
+                    "original_title": "キングダム 大将軍の帰還",
+                    "overview": "Depicts a continuation of the \"Battle of Mayang\", an all-out war against the neighboring country Zhao that Shin and Wang Ki fought in in the previous work \"Flame of Fate\".",
+                    "popularity": 21.6962,
+                    "poster_path": "/qZKKwXyZ92K0mIRpf2FbCkQa7oO.jpg",
+                    "release_date": "2024-07-12",
+                    "title": "Kingdom IV: Return of the Great General",
+                    "video": false,
+                    "vote_average": 6.842,
+                    "vote_count": 520
+                }
+        ];
+        render(<SearchResultContent movieList={movieListData} />);
         const buttons = screen.getAllByRole("button");
         expect(buttons).toBeTruthy();
     });
---
+++ src/component/molecules/SearchResultContent.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import MovieImgButton from "../atoms/MovieImgButton";
+import { Box } from "@mui/material";
+import "../../css/MovieListContent.css"
+
+type Props = {
+    movieList: {
+        adult: boolean,
+        backdrop_path: string | null,
+        genre_ids: number[],
+        id: number,
+        original_language: string,
+        original_title: string,
+        overview: string,
+        popularity: number,
+        poster_path: string | null,
+        release_date: string,
+        title: string,
+        video: boolean,
+        vote_average: number,
+        vote_count: number
+    }[]
+}
+
+const SearchResultContent: React.FC<Props> = ({movieList}) => {
+    const onClickMovieImg = () => {}
+    const baseUrl = "https://image.tmdb.org/t/p/original"
+    return (
+        <Box className="Search-result-box">
+            {movieList.map((movie) => {
+                return (
+                    <Box className="Movie-content-box" key={movie.id}>
+                        <MovieImgButton
+                            srcUrl={movie.poster_path ? `${baseUrl}${movie.poster_path}` : undefined}
+                            alt={movie.title}
+                            onClick={onClickMovieImg}
+                        />
+                    </Box>
+                )
+            })}
+        </Box>
+    )
+}
+
+export default SearchResultContent
