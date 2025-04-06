600be57b1e22446593af0fca7d62ea5dade6ee47
satorukomatsu <sk.comiu@gmail.com>
2025-04-06 17:22:15 +0900

    ルーティングの追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import './css/App.css';
+import Home from './component/pages/Home';
+import SearchResult from './component/pages/SearchResult';
+import { Routes, Route } from 'react-router-dom';
 
-function App() {
+const App:React.FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Sample Movie App</h1>  
-      </header>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/search-result" element={<SearchResult/>}/>
+      </Routes>
     </div>
   );
 }
